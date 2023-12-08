6c3a401168985b39d58be70fd6ae78413f2072ca
Adam Amusa <Adamamusa2004@gmail.com>
2023-12-08 14:24:22 +0000

    Lab10

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -4,17 +4,11 @@ const app = express()
 const port = 4000;
 const cors = require('cors');
 
+//tells location of build folder and static folder where it can get information
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../build')));
+app.use('/static', express.static(path.join(__dirname, 'build//static')));
 
-//allows control access to server
-app.use(cors());
-app.use(function (req, res, next) {
-    //response headers that will print out onto the page
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 // Middleware to parse incoming request bodies
 const bodyParser = require('body-parser');
 // Parse URL-encoded data and populate the req.body object
@@ -96,6 +90,12 @@ app.get('/api/book/:identifier', async(req,res)=>{
     let book = await bookModel.findById(req.params.identifier); //Finds book by id
     res.send(book);//sends the book that was found to the webpage
 })
+
+//for all users not listed above send index.html file
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/../build/index.html'));
+    });
+    
 
 //port listener
 app.listen(port, () => {
